3bfdd101b12175cf610641281b6c73b76da020a9
thanks-cohn <officialmahasbiz@gmail.com>
2026-05-27 13:03:36 -0600

    Modernize file plugin timestamps and idmap calls

--- plugin/file_plugin_common.c
+++ plugin/file_plugin_common.c
@@ -60,7 +60,7 @@ int set_plug_in_inode_common(struct inode *object /* inode to set plugin on */ ,
 	object->i_mode = data->mode;
 	/* this should be plugin decision */
 	object->i_uid = current_fsuid();
-	object->i_mtime = object->i_atime = object->i_ctime = current_time(object);
+	inode_set_mtime_to_ts(object, current_time(object)); inode_set_atime_to_ts(object, current_time(object)); inode_set_ctime_current(object);
 
 	/* support for BSD style group-id assignment. See mount's manual page
 	   description of bsdgroups ext2 mount options for more details */
@@ -256,7 +256,7 @@ int reiser4_add_link_common(struct inode *object, struct inode *parent)
 	 */
 
 	INODE_INC_NLINK(object);
-	object->i_ctime = current_time(object);
+	inode_set_ctime_current(object);
 	return 0;
 }
 
@@ -272,7 +272,7 @@ int reiser4_rem_link_common(struct inode *object, struct inode *parent)
 	 */
 
 	INODE_DROP_NLINK(object);
-	object->i_ctime = current_time(object);
+	inode_set_ctime_current(object);
 	return 0;
 }
 
@@ -292,7 +292,7 @@ int rem_link_common_dir(struct inode *object, struct inode *parent UNUSED_ARG)
 
 	else
 		INODE_DROP_NLINK(object);
-	object->i_ctime = current_time(object);
+	inode_set_ctime_current(object);
 	return 0;
 }
 
@@ -958,7 +958,7 @@ static int process_truncate(struct inode *inode, __u64 size)
 	inode_lock(inode);
 	assert("vs-1704", get_current_context()->trans->atom == NULL);
 	dentry.d_inode = inode;
-	result = inode->i_op->setattr(&init_user_ns, &dentry, &attr);
+	result = inode->i_op->setattr(&nop_mnt_idmap, &dentry, &attr);
 	inode_unlock(inode);
 
 	context_set_commit_async(ctx);
