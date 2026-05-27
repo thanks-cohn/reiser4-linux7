ca85d2937d0a2c0576a470b5ea74e31f662cf25c
thanks-cohn <officialmahasbiz@gmail.com>
2026-05-27 14:19:13 -0600

    Implement Linux 7 fs_context mount bridge

--- compat/fs_context_stub.c
+++ compat/fs_context_stub.c
@@ -1,8 +1,20 @@
-
 #include <linux/fs_context.h>
 #include <linux/fs.h>
+#include <linux/mount.h>
+
+#include "../super.h"
+
+static int reiser4_get_tree(struct fs_context *fc)
+{
+    return get_tree_bdev(fc, reiser4_fill_super);
+}
+
+static const struct fs_context_operations reiser4_context_ops = {
+    .get_tree = reiser4_get_tree,
+};
 
 int reiser4_init_fs_context(struct fs_context *fc)
 {
-    return -EOPNOTSUPP;
+    fc->ops = &reiser4_context_ops;
+    return 0;
 }
